64227d1967e18d6c18eebf3ab8f3467db0e891b8
yk <yefim_korshever@mail.ru>
2023-03-14 23:16:01 +0300

    Add basic food app models
    Dish, DishItem, Product, Sale, Subscription

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dish, DishItem, Product, Sale, Subscription
+
+
+class DishItemInline(admin.TabularInline):
+    model = DishItem
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+    list_display = [
+        'title',
+        'description',
+    ]
+    inlines = [DishItemInline]
+
+
+@admin.register(DishItem)
+class DishItemAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+    list_display = [
+        'dish',
+        'product',
+        'quantity',
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+    list_display = [
+        'title',
+        'price',
+    ]
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+    list_display = [
+        'sum',
+        'payed_at',
+    ]
+    pass
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+
+    list_display = [
+        'title',
+        'duration',
+        'price',
+    ]
--- food/models.py
+++ food/models.py
@@ -1,3 +1,108 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(
+        verbose_name='название',
+        max_length=50
+    )
+    price = models.DecimalField(
+        'цена',
+        max_digits=15,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return self.title
+
+
+class Dish(models.Model):
+    title = models.CharField(
+        verbose_name='название',
+        max_length=50
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True,
+    )
+    image = models.ImageField(
+        'картинка',
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'блюдо'
+        verbose_name_plural = 'блюда'
+
+    def __str__(self):
+        return self.title
+
+
+class DishItem(models.Model):
+    dish = models.ForeignKey(
+        Dish,
+        related_name='items',
+        verbose_name="блюдо",
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        Product,
+        related_name='dish_items',
+        verbose_name='продукт',
+        on_delete=models.PROTECT,
+    )
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name='количество',
+        validators=[MinValueValidator(1)],
+    )
+
+    class Meta:
+        verbose_name = 'ингридиент'
+        verbose_name_plural = 'ингридиенты'
+
+
+class Subscription(models.Model):
+    title = models.CharField(
+        verbose_name='название',
+        max_length=50
+    )
+    duration = models.PositiveSmallIntegerField(
+        verbose_name='количество',
+        validators=[MinValueValidator(1)],
+    )
+    price = models.DecimalField(
+        'цена',
+        max_digits=15,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
+
+    def __str__(self):
+        return self.title
+
+
+class Sale(models.Model):
+    sum = models.DecimalField(
+        'цена',
+        max_digits=15,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
+    payed_at = models.DateTimeField(
+        verbose_name='дата оплаты',
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = 'продажа'
+        verbose_name_plural = 'продажи'
