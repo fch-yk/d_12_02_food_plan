dbafd63fd5fec2cacd166b6b8aeda0c14a37aabb
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-03-15 21:57:34 +0200

    Fix migrations error and fix urls

--- food/urls.py
+++ food/urls.py
@@ -4,6 +4,6 @@ from .views import view_index, show_order
 app_name = 'food'
 
 urlpatterns = [
-    path('', view_index, name='index').
+    path('', view_index, name='index'),
     path('order/', show_order, name='show_order'),
 ]
