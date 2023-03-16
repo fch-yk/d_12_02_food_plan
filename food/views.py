b6d953ba5a308b2f382e856bd3a08a9f484ddfd1
yk <yefim_korshever@mail.ru>
2023-03-16 10:30:41 +0300

    Remove unused static files

--- food/views.py
+++ food/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def view_index(request):
     return render(request, 'food/index.html')
 
+
 def show_order(request):
     context = {}
     return render(request, 'order.html', context=context)
-
