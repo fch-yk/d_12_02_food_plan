1f6e3e0790b14a966b2ba3f48aebca137620943b
yk <yefim_korshever@mail.ru>
2023-03-16 18:56:49 +0300

    Return django-debug-toolbar

--- food_plan/settings.py
+++ food_plan/settings.py
@@ -44,10 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'food',
-    'users'
+    'users',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
