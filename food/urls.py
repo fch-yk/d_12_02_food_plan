0d578abf91d4c38e5acab8814fdd4048cc29b3ce
yk <yefim_korshever@mail.ru>
2023-03-16 14:50:07 +0300

    Add post request for an order

--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import view_index, show_order
+from .views import view_index, show_order, post_order
 
 app_name = 'food'
 
 urlpatterns = [
     path('', view_index, name='index'),
     path('order/', show_order, name='show_order'),
+    path('order/postorder', post_order, name='post_order'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def view_index(request):
@@ -8,3 +9,11 @@ def view_index(request):
 def show_order(request):
     context = {}
     return render(request, 'order.html', context=context)
+
+
+def post_order(request):
+    # получаем из данных запроса POST отправленные через форму данные
+    # name = request.POST.get("name", "Undefined")
+    # age = request.POST.get("age", 1)
+    # return HttpResponse(f"<h2>Name: {name}  Age: {age}</h2>")
+    return HttpResponse("Hello")
