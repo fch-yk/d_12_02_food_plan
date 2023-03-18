169babb1df3c5ef76cecaf7e2e565a8d27e75f47
yk <yefim_korshever@mail.ru>
2023-03-18 15:17:17 +0300

    Save allergies for a user

--- food/views.py
+++ food/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.views import View
 
-from food.models import Dish
+from food.models import Dish, DishCategory
 
 
 class Index(View):
@@ -18,19 +19,36 @@ class Index(View):
 
 
 def show_order(request):
-    context = {}
+    dish_categories = DishCategory.objects.all()
+    context = {
+        'dish_categories': dish_categories
+    }
     return render(request, 'food/order.html', context=context)
 
 
 def post_order(request):
-    # получаем из данных запроса POST отправленные через форму данные
-    # name = request.POST.get("name", "Undefined")
-    # age = request.POST.get("age", 1)
-    # return HttpResponse(f"<h2>Name: {name}  Age: {age}</h2>")
-    context = {}
+
+    allergies_ids = []
+    dish_categories = DishCategory.objects.all()
+    for dish_category in dish_categories:
+        allergy_id = request.POST.get(f"allergy_{dish_category.id}")
+        if allergy_id:
+            allergies_ids.append(allergy_id)
+
+    allergies = ','.join(allergies_ids)
+
+    context = {'allergies': allergies}
     return render(request, 'food/payment.html', context=context)
 
 
 def pay(request):
+    user = get_user_model().objects.get(id=request.user.id)
+    allergies = request.POST.get('allergies')
+
+    if allergies:
+        allergies_ids = [int(id) for id in allergies.split(',')]
+        allergies = DishCategory.objects.filter(id__in=allergies_ids)
+        user.allergy_to.set(allergies)
+
     context = {}
     return render(request, 'food/payment_success.html', context=context)
